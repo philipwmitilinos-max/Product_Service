556bc2a92b5f01c73701e505a7e7ca882cd0e3e7
philipwmitilinos-max <philipwmitilinos@gmail.com>
2026-05-19 13:45:49 +0200

    Impelment - model - Product.

---
+++ src/main/java/se/iths/philip/product_service/model/Product.java
@@ -0,0 +1,22 @@
+package se.iths.philip.product_service.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int stock;
+}
