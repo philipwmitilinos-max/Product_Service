d58d7bb19a120145aceb64d2f73e439ccc820131
philipwmitilinos-max <philipwmitilinos@gmail.com>
2026-05-21 11:13:06 +0200

    Implement - controller - ProductController.

---
+++ src/main/java/se/iths/philip/product_service/controller/ProductController.java
@@ -0,0 +1,46 @@
+package se.iths.philip.product_service.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import se.iths.philip.product_service.dto.ProductRequestDTO;
+import se.iths.philip.product_service.dto.ProductResponseDTO;
+import se.iths.philip.product_service.dto.ProductStockRequest;
+import se.iths.philip.product_service.service.ProductService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponseDTO createProduct(
+            @Valid @RequestBody ProductRequestDTO dto) {
+
+        return service.createProduct(dto);
+    }
+
+    @GetMapping
+    public List<ProductResponseDTO> getAllProducts() {
+
+        return service.getAllProducts();
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable Long id) {
+        service.deleteProduct(id);
+    }
+
+    @PostMapping("/stock/decrease")
+    public List<ProductResponseDTO> decreaseStock(
+            @RequestBody List<ProductStockRequest> requests) {
+        return service.decreaseStock(requests);
+    }
+}
