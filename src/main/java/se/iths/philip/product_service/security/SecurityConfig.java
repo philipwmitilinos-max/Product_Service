becf12c94fe1810b573ab45d4944d4f56543092e
philipwmitilinos-max <philipwmitilinos@gmail.com>
2026-05-22 13:55:42 +0200

    Implement - application.properties.
    Jag har inte kopplat de här till någon databas.
    Eller någon jwt token.

--- src/main/java/se/iths/philip/product_service/security/SecurityConfig.java
+++ src/main/java/se/iths/philip/product_service/security/SecurityConfig.java
@@ -29,7 +29,9 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/products/stock/decrease")
                         .hasAnyRole("ADMIN", "USER")
 
-                        .anyRequest().authenticated()
+                        .anyRequest()
+                                .authenticated()
+//                                .permitAll()
                 )
                 .oauth2ResourceServer(oauth2 ->
                         oauth2.jwt(jwt -> {}));
